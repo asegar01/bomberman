75ac805cb4e32b03093b345a2435d9feeb9d4b0a
asegar01 <asegar01@ucm.es>
2023-05-20 18:22:31 +0200

    Mejoras mapa y jugador

--- Bomberman/Assets/Scripts/PlayerMovement.cs
+++ Bomberman/Assets/Scripts/PlayerMovement.cs
@@ -11,10 +11,12 @@ public class PlayerMovement : MonoBehaviour
     public int indestructibleLayer = 2;
 
     private Vector3Int currentCell;       // Celda actual en la que se encuentra el jugador
+    private Animation anim;
 
     private void Start()
     {
         currentCell = grid.WorldToCell(transform.position);
+        anim = GetComponent<Animation>();
     }
 
     private void Update()
@@ -30,14 +32,35 @@ public class PlayerMovement : MonoBehaviour
         // Obtener la celda correspondiente al movimiento
         Vector3Int targetCell = grid.WorldToCell(targetPos);
 
+        // Calcular el centro de la celda objetivo
+        Vector3 targetCellCenter = grid.GetCellCenterWorld(targetCell);
+
         // Rotacion del jugador
-        Vector3 dir = (targetPos - transform.position).normalized;
+        Vector3 dir = (targetCellCenter - transform.position).normalized;
         Quaternion targetRotation = Quaternion.LookRotation(dir);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
+        // Animacion del jugador
+        //if (Vector3.Distance(transform.position, targetCellCenter) > 0.1f)
+        //{
+        //    // Reproducir la animacion de movimiento
+        //    if (!anim.isPlaying || anim.clip.name != "Run")
+        //    {
+        //        anim.Play("Run");
+        //    }
+        //}
+        //else
+        //{
+        //    // Reproducir la animacion de inactividad
+        //    if (!anim.isPlaying || anim.clip.name != "Idle")
+        //    {
+        //        anim.Play("Idle");
+        //    }
+        //}
+
         // Mover el jugador a la celda objetivo
-        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, targetPos) <= 0.1f)
+        transform.position = Vector3.Lerp(transform.position, targetCellCenter, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, targetCellCenter) <= 0.1f)
             currentCell = targetCell;
     }
 }
