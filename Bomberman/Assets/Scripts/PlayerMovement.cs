7c31b187c83eefd91ee2946b18a12fe8999d2e27
asegar01 <asegar01@ucm.es>
2023-05-20 22:31:09 +0200

    Mejoras movimiento jugador

--- Bomberman/Assets/Scripts/PlayerMovement.cs
+++ Bomberman/Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,21 @@ public class PlayerMovement : MonoBehaviour
     public Grid grid;
     public float moveSpeed = 20f;          // Velocidad de movimiento del jugador
     public float rotationSpeed = 10f;      // Velocidad de rotacion del jugador
-    public int destructibleLayer = 1;
-    public int indestructibleLayer = 2;
+    public int destructibleLayer = 6;
+    public int indestructibleLayer = 7;
+    private float playerHeight;
 
     private Vector3Int currentCell;       // Celda actual en la que se encuentra el jugador
-    private Animation anim;
+    //private Animation anim;
 
     private void Start()
     {
         currentCell = grid.WorldToCell(transform.position);
-        anim = GetComponent<Animation>();
+        playerHeight = transform.position.y;
+        //anim = GetComponent<Animation>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         // Input del jugador
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -27,18 +29,19 @@ public class PlayerMovement : MonoBehaviour
 
         // Direccion del movimiento
         Vector3 movementDir = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-        Vector3 targetPos = transform.position + movementDir * moveSpeed * Time.deltaTime;
+        Vector3 targetPos = transform.position + movementDir * moveSpeed * Time.fixedDeltaTime;
 
         // Obtener la celda correspondiente al movimiento
         Vector3Int targetCell = grid.WorldToCell(targetPos);
 
         // Calcular el centro de la celda objetivo
         Vector3 targetCellCenter = grid.GetCellCenterWorld(targetCell);
+        targetCellCenter.y = playerHeight;
 
         // Rotacion del jugador
         Vector3 dir = (targetCellCenter - transform.position).normalized;
         Quaternion targetRotation = Quaternion.LookRotation(dir);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
 
         // Animacion del jugador
         //if (Vector3.Distance(transform.position, targetCellCenter) > 0.1f)
@@ -58,9 +61,29 @@ public class PlayerMovement : MonoBehaviour
         //    }
         //}
 
+        bool canMove = true;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, dir, out hit) && hit.collider != null)
+        {
+            if (dir.x > 0f && hit.distance < 0.5f || dir.x < 0f && hit.distance < 0.65f)
+                canMove = false;
+            if (dir.z > 0f && hit.distance < 0.6f || dir.z < 0f && hit.distance < 0.5f)
+                canMove = false;
+        }
+
         // Mover el jugador a la celda objetivo
-        transform.position = Vector3.Lerp(transform.position, targetCellCenter, moveSpeed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, targetCellCenter) <= 0.1f)
-            currentCell = targetCell;
+        if (canMove)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetCellCenter, moveSpeed * Time.fixedDeltaTime);
+            if (Vector3.Distance(transform.position, targetCellCenter) <= 0.1f)
+                currentCell = targetCell;
+        }
+        else
+        {
+            Vector3 targetCellCx = grid.GetCellCenterWorld(currentCell);
+            targetCellCx.y = playerHeight;
+            transform.position = Vector3.Lerp(transform.position, targetCellCx, moveSpeed * Time.fixedDeltaTime);
+        }
     }
 }
