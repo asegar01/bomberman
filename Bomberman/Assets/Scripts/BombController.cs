fb246b82af704ac6386a270cd16019bc6263564d
asegar01 <asegar01@ucm.es>
2023-05-21 18:16:36 +0200

    Explosiones

--- Bomberman/Assets/Scripts/BombController.cs
+++ Bomberman/Assets/Scripts/BombController.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class BombController : MonoBehaviour
 {
+    [Header("Bomb")]
     public GameObject bombPrefab;
     private KeyCode inputKey = KeyCode.Space;
     public float bombFuseTime = 3f;
     public int bombsRemaining = 3;
     PlayerMovement playerMovement;
-    private float offset = 0.25f;
+    private float offset = 0.3f;
+
+    [Header("Explosion")]
+    public GameObject explosionPrefab;
+    public float explosionDuration = 1f;
+    public int explosionRadius = 2;
 
     private void Start()
     {
@@ -24,24 +30,52 @@ public class BombController : MonoBehaviour
         }
     }
 
+    // Instancia una bomba en la posicion del jugador
     private IEnumerator CreateBomb()
     {
         Vector3 currentCell = playerMovement.GetCurrentCell();
-        currentCell.x -= offset;
-        currentCell.z += offset;
-        GameObject bomb = Instantiate(bombPrefab, currentCell, Quaternion.identity);
+        Vector3 spawnPoint = currentCell;
+        spawnPoint.x -= offset;
+        spawnPoint.z += offset;
+        GameObject bomb = Instantiate(bombPrefab, spawnPoint, Quaternion.identity);
         bombsRemaining--;
 
         yield return new WaitForSeconds(bombFuseTime);
+
+        GameObject explosion = Instantiate(explosionPrefab, currentCell, Quaternion.Euler(-90f, 0f, 180f));
+        Destroy(explosion, explosionDuration);
+
+        Explode(currentCell, Vector3.forward, explosionRadius);
+        Explode(currentCell, Vector3.right, explosionRadius);
+        Explode(currentCell, Vector3.back, explosionRadius);
+        Explode(currentCell, Vector3.left, explosionRadius);
 
         Destroy(bomb);
         bombsRemaining++;
     }
 
+    // Explosion de la bomba
+    private void Explode(Vector3 position, Vector3 direction, int length)
+    {
+        if (length <= 0) return;
+
+        position += direction;
+        Collider[] colliders = Physics.OverlapSphere(position, 0.1f);
+        foreach (Collider collider in colliders)
+        {
+            // Verificar si el objeto se puede atravesar
+            if (collider.gameObject.layer == LayerMask.NameToLayer("Unbreakable"))
+                return;
+        }
+
+        GameObject explosion = Instantiate(explosionPrefab, position, Quaternion.Euler(-90f, 0f, 180f));
+        Destroy(explosion, explosionDuration);
+
+        Explode(position, direction, length - 1);
+    }
+
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("trigger");
-
         if(other.gameObject.layer == LayerMask.NameToLayer("Bomb"))
         {
             other.isTrigger = false;
